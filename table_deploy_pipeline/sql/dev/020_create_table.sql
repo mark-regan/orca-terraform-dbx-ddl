05c8ad6ae6112e5a29be310d1d4739606d461ca3
Mark Regan <mark.regan@cumberland.co.uk>
2025-09-11 15:03:28 +0100

    Updated

--- table_deploy_pipeline/sql/dev/020_create_table.sql
+++ table_deploy_pipeline/sql/dev/020_create_table.sql
@@ -1,8 +1,10 @@
 -- tags: ddl,tables
-USE CATALOG ${CATALOG_ORCA};
-USE SCHEMA ${SCHEMA_ORCA_METADATA};
-CREATE TABLE IF NOT EXISTS ${SCHEMA_ORCA_METADATA}.example_table (
+
+CREATE TABLE IF NOT EXISTS ${CATALOG_ORCA}.${SCHEMA_ORCA_METADATA}.example_table3 (
   id BIGINT,
   name STRING
 );
 
+GRANT SELECT, MODIFY ON ${CATALOG_ORCA}.${SCHEMA_ORCA_METADATA}.example_table3 to `Azure_MIReporting_Databricks_Orca_Exec_Dev`;
+GRANT SELECT, MODIFY ON ${CATALOG_ORCA}.${SCHEMA_ORCA_METADATA}.example_table3 to `Azure_MIReporting_Databricks_Orca_Support_Dev`;
+GRANT SELECT, MODIFY ON ${CATALOG_ORCA}.${SCHEMA_ORCA_METADATA}.example_table3 to `Azure_MIReporting_Admin_Dev`;
--- table_deploy_pipeline/sql/dev/900_seed_data.sql
+++ table_deploy_pipeline/sql/dev/900_seed_data.sql
@@ -1,6 +1,5 @@
 -- tags: dml,seed
-USE CATALOG ${CATALOG_ORCA};
-USE SCHEMA ${SCHEMA_ORCA_METADATA};
-INSERT INTO ${SCHEMA_ORCA_METADATA}.example_table (id, name)
+
+INSERT INTO ${CATALOG_ORCA}.${SCHEMA_ORCA_METADATA}.example_table3 (id, name)
 VALUES (1, 'alpha'), (2, 'beta');
 
